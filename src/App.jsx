7c2de66d47a8d785d57c45c3f605b4a7b82d4d03
434huzaifa <saadhuzaifa2497@gmail.com>
2023-12-11 16:18:39 +0600

    initial

---
+++ src/App.jsx
@@ -0,0 +1,14 @@
+import Hero from './Hero'
+import NavBar from './NavBar'
+
+function App() {
+
+  return (
+    <>
+    <NavBar></NavBar>
+    <Hero></Hero>
+    </>
+  )
+}
+
+export default App
---
+++ src/Hero.jsx
@@ -0,0 +1,17 @@
+
+const Hero = () => {
+    return (
+        <div className="flex gap-2 justify-between">
+            <div>
+                <p>Hi,I am <span>Huzaifa</span></p>
+                <p>Full Stack Developer</p>
+                <p>Hi, i am Md. Huzaifa. I am a fullstack Developer with profficiency in Django</p>
+            </div>
+            <div>
+                <img src="/my.png" alt="" />
+            </div>
+        </div>
+    );
+};
+
+export default Hero;
